8ea15b0f0ceb0890872dbbb6a79d698b512afdcf
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-04 16:47:17 +0100

    some code

---
+++ src/handling_input.py
@@ -0,0 +1,4 @@
+def commands_handling(user_input):
+    if 'currency exchange' in user_input or 'global currency' in user_input or 'exchange' in user_input or 'USD' in user_input:
+        print('hello i know a little bit about global currencies , how can i assit you')
+    
