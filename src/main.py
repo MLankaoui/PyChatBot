e4fe0ceb51a35b2f247a7ff3f9b6f67c591e3dd6
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-04 15:53:07 +0100

    first user inputs handled, first cases

---
+++ src/main.py
@@ -0,0 +1,25 @@
+def main():
+    greetings()
+
+
+    while True:
+        user_input = input('-> ').lower()
+
+        if 'hello' in user_input:
+            print('hello how can i assist you today')
+
+        elif 'hru' in user_input or 'how are you' in user_input:
+            print('hello , i don\'t experience emotions, how about you , you feeling good today?')
+
+        elif 'im good' in user_input or 'i\'m alright' in user_input or 'alright' in user_input or 'good' in user_input:
+            print('good to know!')
+
+        elif 'exshausted' in user_input or 'bad' in user_input or 'tired' in user_input or 'not feeling good' in user_input or 'not feeling well' in user_input:
+            print('im sorry to hear that , how can i help you?')
+        else:
+            pass
+
+def greetings():
+    print('hello welcome to PYCHATBOT')
+
+main()
