7a62a5b7c8f2ae66ff121103e9eef9f320a86042
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-05 09:19:02 +0100

    quick fix displaying weather infos for all cities instead of just moroccan cities

--- src/weather_api/weather_infos.py
+++ src/weather_api/weather_infos.py
@@ -8,7 +8,7 @@ api_key = os.getenv('API_KEY_WEATHER')
 def display_weather_infos(city_name):
     print(f'So you chose to display the weather infos of {city_name} city.')
 
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name},ma&appid={api_key}")
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}")
 
     if response.status_code == 404:
         print(f"The city '{city_name}' is not available for the moment!")
