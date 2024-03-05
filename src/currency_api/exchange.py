9be9e8a18e89b414f384a8da8b81dc6f887b856c
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-05 08:47:32 +0100

    feature to exchange currency based on the ammount added to the bot

--- src/currency_api/exchange.py
+++ src/currency_api/exchange.py
@@ -8,6 +8,7 @@ api_key = os.getenv('API_KEY_EXCHANGE')
 def exchange(user_currency, target_currency, amount):
     print(f'nice you chose {user_currency}/{target_currency} , with the amount of {amount}')
 
+
     currency = requests.get(f"https://api.currencyapi.com/v3/latest",params={"apikey": api_key, "base_currency": target_currency})
 
     exchanges = currency.json()
--- src/handling_input.py
+++ src/handling_input.py
@@ -6,27 +6,23 @@ from currency_api.exchange import exchange
 load_dotenv()
 api_key = os.getenv('API_KEY_EXCHANGE')
 
-users_currency = ''
-target_currency = ''
-amount = ''
 
 def commands_handling(user_input):
     global users_currency, target_currency, amount
-    if 'currency exchange' in user_input or 'global currency' in user_input or 'exchange' in user_input or 'USD' in user_input:
+
+    if 'currency exchange' in user_input or 'global currency' in user_input or 'exchange' in user_input or 'USD' in user_input or 'money' in user_input or 'trade' in user_input:
         print('hello i know a little bit about global currencies , how can i assist you')
-    
-    currency = requests.get(f"https://api.currencyapi.com/v3/latest",params={"apikey": api_key, "base_currency": users_currency})
+        users_currency = input('-> please enter your current currency: ').upper()
+        target_currency = input('-> please enter your target currency: ').upper()
+        amount = input('please enter the ammount : ')
 
-    exchanges = currency.json()
-
-    for currency_one in user_input.split():
-        if currency_one in exchanges['data']:
-            users_currency = currency_one
-            for currency_two in user_input.split():
-                if currency_two in exchanges['data']:
-                    target_currency = currency_two
-                    for currency_amount in user_input.split():
-                        if currency_amount.isdigit():
-                            amount = currency_amount
-
+                            
+    print(f'ammount: {amount}')
+    print(f'users_currency: {users_currency}')
+    print(f'target currency: {target_currency}')
     exchange(users_currency, target_currency, amount)
+
+
+
+
+
