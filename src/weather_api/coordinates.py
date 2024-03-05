54d05b79addf9dfa54bba8537ff4f4b90b078e60
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-05 08:54:36 +0100

    added the api key for the weather api, to add weather features to the bot

---
+++ src/weather_api/coordinates.py
@@ -0,0 +1,21 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # take environment variables from .env.
+api_key = os.getenv('API_KEY_WEATHER')
+
+
+def display_city_cooredinates(city_name):
+    print('So you chose to display the coordinates of your chosen city.')
+
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}")
+
+    if response.status_code == 404:
+        print(f"The city '{city_name}' is not available for the moment!")
+        return
+
+    weather = response.json()
+
+    print(f"Longitude: {weather['coord']['lon']}")
+    print(f"Latitude: {weather['coord']['lat']}")
